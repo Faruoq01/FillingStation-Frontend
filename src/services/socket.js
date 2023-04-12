4f250c85e4a7346696ff591bec43119c45d1e5e9
Faruoq01 <59730816+Faruoq01@users.noreply.github.com>
2023-04-12 12:42:17 +0100

    Update socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,4 @@
 import { io } from 'socket.io-client';
+import config from '../constants';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:5000';
-
-export const socket = io(URL);
+export const socket = io(config.BASE_URL);
