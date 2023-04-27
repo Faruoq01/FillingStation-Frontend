f5f742f47a79cfad6daf6df41e3fde3929f2d3a6
Faruoq01 <59730816+Faruoq01@users.noreply.github.com>
2023-04-27 10:43:08 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // BASE_URL: "https://360station.co",
-    BASE_URL: "http://localhost:5000",
+    BASE_URL: "https://360station.co",
+    // BASE_URL: "http://localhost:5000",
     API_KEY: "AIzaSyDZnZ15rSQS_2CluQE47CY5MRqAHGdUYZY",
 }
