f5c5630f429fc830ab4892f12d13a358d5181469
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-06-14 14:54:58 +0100

    update

--- src/components/wallet-list/WalletPaymentListIndex.js
+++ src/components/wallet-list/WalletPaymentListIndex.js
@@ -1,8 +1,9 @@
 import React, { Fragment } from "react";
 
-import "../../styles/estation/individual_sale.scss";
+import "../../styles/estation/payment.scss";
 import { useMediaQuery } from "@mui/material";
 import WalletPaymentTable from "./WalletPaymentTable";
+import Button from "./Button";
 
 export default function WalletPaymentListIndex() {
   const mobile = useMediaQuery("(max-width:900px)");
@@ -10,13 +11,29 @@ export default function WalletPaymentListIndex() {
   return (
     <div className="individual-sale-container-">
       <div className="wrap-btn-wrap">
-        <input
-          className="search-"
-          type="text"
-          id="fname"
-          placeholder="Search"
-          style={{ height: 30, border: "1.5px solid #C6C6C6" }}
-        />
+        <div className="btn-wrap-">
+          <Button>Individual</Button>
+          <Button style={{ marginLeft: 5 }}>Corporate</Button>
+        </div>
+        <div className="input-wrapp-payment">
+          <input
+            className="search-"
+            type="text"
+            id="fname"
+            placeholder="Search"
+            style={{ height: 30, border: "1.5px solid #C6C6C6" }}
+          />
+          <Button
+            styles={{
+              marginLeft: 2,
+              height: 30,
+              backgroundColor: "#0F88F2",
+              color: "white",
+            }}
+          >
+            Register
+          </Button>
+        </div>
       </div>
       <WalletPaymentTable />
     </div>
