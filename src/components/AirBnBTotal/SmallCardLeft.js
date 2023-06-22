b06e902ad6d69d874fc2251d8d339b52f96505fc
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-06-22 12:00:19 +0100

    update

--- src/components/AirBnBTotal/SmallCardLeft.js
+++ src/components/AirBnBTotal/SmallCardLeft.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./airbnb.scss";
 export default function SmallCardLeft() {
-  return <div className="left-card"></div>;
+  return (
+    <div className="left-card">
+      <div className="left-card-inner"></div>
+    </div>
+  );
 }
