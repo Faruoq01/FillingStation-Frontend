d2c453039add55d3e97368598273f0aec877c8df
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-07-24 15:29:16 +0100

    update

--- src/components/comprehensive_report/components/CustomTable9.js
+++ src/components/comprehensive_report/components/CustomTable9.js
@@ -1,39 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-
-const ApproximateDecimal = (data) => {
-  const changeToString = String(data);
-  let prefix = Number(data) < 0 ? "-" : "";
-
-  const findIndex = changeToString.indexOf(".");
-  if (findIndex === -1) {
-    const rem = changeToString.replace(/[^0-9.]/g, "");
-    return `${prefix}${Number(rem).toLocaleString()}`;
-  } else {
-    let splitByDecimal = changeToString.split(".");
-
-    ////////// left hand side //////////////////////
-    let removeSpecialCharacters = splitByDecimal[0].replace(/[^0-9.]/g, "");
-    let addCommaToLeftSide = Number(removeSpecialCharacters).toLocaleString();
-
-    ////////// right hand side /////////////////////
-    let removeCharactersRight = splitByDecimal[1].replace(/[^0-9.]/g, "");
-    if (removeCharactersRight.length <= 2) {
-      return `${prefix}${addCommaToLeftSide.concat(
-        ".",
-        removeCharactersRight
-      )}`;
-    } else {
-      let tenths =
-        Number(removeCharactersRight.charAt(2)) < 5
-          ? removeCharactersRight.charAt(1)
-          : String(Number(removeCharactersRight.charAt(1)) + 1);
-      let newRight = removeCharactersRight.charAt(0).concat("", tenths);
-
-      return `${prefix}${addCommaToLeftSide.concat(".", newRight)}`;
-    }
-  }
-};
+import ApproximateDecimal from "../../common/approx";
 
 export default function CustomTable9({
   header = [],
