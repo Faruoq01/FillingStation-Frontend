5cb8564d9c1fd657b1d4f4c0fd68f0307257aa1d
Faruoq01 <59730816+Faruoq01@users.noreply.github.com>
2023-08-29 23:55:48 +0100

    update

--- src/components/Settings/permissionsHelper.js
+++ src/components/Settings/permissionsHelper.js
@@ -1,203 +1,203 @@
 const data = [
-    {
-        name: "Dashboard",
-        permissions: {
-            '0': "Selecting a date range.",
-            '1': "selecting a single station",
-            '2': "selecting all stations",
-            '3': "viewing organisational staffs on a single station",
-            '4': "viewing organisational staffs on all stations",
-            '5': "viewing total sales",
-            '6': "viewing tanks and pumps(assets)",
-            '7': "viewing supply page",
-            '8': "viewing expenses",
-            '9': "viewing incoming order",
-            '10': "selecting top performing stations",
-        }
+  {
+    name: "Dashboard",
+    permissions: {
+      0: "selecting a single station",
+      1: "selecting all stations",
+      2: "Selecting a date range.",
+      3: "viewing organisational staffs on a single station",
+      4: "viewing organisational staffs on all stations",
+      5: "viewing total sales",
+      6: "viewing tanks and pumps(assets)",
+      7: "viewing supply page",
+      8: "viewing expenses",
+      9: "viewing incoming order",
+      10: "selecting top performing stations",
     },
+  },
 
-    {
-        name: "Daily Sales",
-        permissions: {
-            '0': "selecting a single station",
-            '1': "selecting all stations",
-            '2': "viewing comprehensive reports",
-            '3': "changing daily sales date",
-            '4': "viewing task list",
-            '5': "viewing daily supllies",
-            '6': "viewing daily payments",
-            '7': "viewing daily lpo sales",
-            '8': "viewing daily incoming order",
-            '9': "Allow users to print report on comprehensive report page",
-            '10': "Allow users to view reports from other dates",
-            '11': "Users to view and update supply",
-            '12': "users to update pump sales",
-            '13': "users to update return to tank",
-            '14': "users to update LPO",
-            '15': "users to update expenses",
-            '16': "users to update payments",
-            '17': "users to update dipping"
-        }
+  {
+    name: "Daily Sales",
+    permissions: {
+      0: "selecting a single station",
+      1: "selecting all stations",
+      2: "viewing comprehensive reports",
+      3: "changing daily sales date",
+      4: "viewing task list",
+      5: "viewing daily supllies",
+      6: "viewing daily payments",
+      7: "viewing daily lpo sales",
+      8: "viewing daily incoming order",
+      9: "Allow users to print report on comprehensive report page",
+      10: "Allow users to view reports from other dates",
+      11: "Users to view and update supply",
+      12: "users to update pump sales",
+      13: "users to update return to tank",
+      14: "users to update LPO",
+      15: "users to update expenses",
+      16: "users to update payments",
+      17: "users to update dipping",
     },
+  },
 
-    {
-        name: "My Stations",
-        permissions: {
-            '0': "Creating stations",
-            '1': "Viewing tanks",
-            '2': "Creating tanks",
-            '3': "Creating pumps",
-            '4': "Viewing pumps",
-            '5': "Editing tanks",
-            '6': "Deleting tanks",
-            '7': "Editing pumps",
-            '8': "Deleting pumps",
-        }
+  {
+    name: "My Stations",
+    permissions: {
+      0: "Creating stations",
+      1: "Viewing tanks",
+      2: "Creating tanks",
+      3: "Creating pumps",
+      4: "Viewing pumps",
+      5: "Editing tanks",
+      6: "Deleting tanks",
+      7: "Editing pumps",
+      8: "Deleting pumps",
     },
+  },
 
-    {
-        name: "Record Sales",
-        permissions: {
-            '0': "Selecting a single station",
-            '1': "Selecting all stations",
-            '2': "Selecting date",
-            '3': "Saving pump update",
-            '4': "Saving return to tank",
-            '5': "Saving LPO",
-            '6': "Saving expenses",
-            '7': "Saving payments",
-            '8': "Saving dipping",
-        }
+  {
+    name: "Record Sales",
+    permissions: {
+      0: "Selecting a single station",
+      1: "Selecting all stations",
+      2: "Selecting date",
+      3: "Saving pump update",
+      4: "Saving return to tank",
+      5: "Saving LPO",
+      6: "Saving expenses",
+      7: "Saving payments",
+      8: "Saving dipping",
     },
+  },
 
-    {
-        name: "Analysis",
-        permissions: {
-            '0': "Selecting a single station",
-            '1': "Selecting all stations",
-            '2': "Changing cost price",
-            '3': "Changing selling price",
-            '4': "Viewing expenses",
-            '5': "Viewing payments",
-            '6': "Selecting date range",
-        }
+  {
+    name: "Analysis",
+    permissions: {
+      0: "Selecting a single station",
+      1: "Selecting all stations",
+      2: "Changing cost price",
+      3: "Changing selling price",
+      4: "Viewing expenses",
+      5: "Viewing payments",
+      6: "Selecting date range",
     },
+  },
 
-    {
-        name: "Payments",
-        permissions: {
-            '0': "Selecting a single station",
-            '1': "Selecting all stations",
-            '2': "Payment confirmation function",
-        }
+  {
+    name: "Payments",
+    permissions: {
+      0: "Selecting a single station",
+      1: "Selecting all stations",
+      2: "Payment confirmation function",
     },
+  },
 
-    {
-        name: "Expenses",
-        permissions: {
-            '0': "Selecting a single station",
-            '1': "Selecting all stations",
-        }
+  {
+    name: "Expenses",
+    permissions: {
+      0: "Selecting a single station",
+      1: "Selecting all stations",
     },
+  },
 
-    {
-        name: "Corporate Sales",
-        permissions: {
-            '0': "Selecting a single station",
-            '1': "Selecting all stations",
-            '2': "Creating new LPO",
-            '3': "Viewing lpo list",
-            '4': "Editing LPO rates",
-            '5': "Print lpo record",
-        }
+  {
+    name: "Corporate Sales",
+    permissions: {
+      0: "Selecting a single station",
+      1: "Selecting all stations",
+      2: "Creating new LPO",
+      3: "Viewing lpo list",
+      4: "Editing LPO rates",
+      5: "Print lpo record",
     },
+  },
 
-    {
-        name: "Product Orders",
-        permissions: {
-            '0': "Selecting a single station",
-            '1': "Selecting all stations",
-            '2': "Creating new product order",
-            '3': "Print product order record",
-        }
+  {
+    name: "Product Orders",
+    permissions: {
+      0: "Selecting a single station",
+      1: "Selecting all stations",
+      2: "Creating new product order",
+      3: "Print product order record",
     },
+  },
 
-    {
-        name: "Incoming Orders",
-        permissions: {
-            '0': "Selecting a single station",
-            '1': "Selecting all stations",
-            '2': "Creating new incoming order",
-            '3': "Print incoming order record",
-        }
+  {
+    name: "Incoming Orders",
+    permissions: {
+      0: "Selecting a single station",
+      1: "Selecting all stations",
+      2: "Creating new incoming order",
+      3: "Print incoming order record",
     },
+  },
 
-    {
-        name: "Supply",
-        permissions: {
-            '0': "Selecting a single station",
-            '1': "Selecting all stations",
-            '2': "Creating new supply",
-            '3': "Print supply record",
-        }
+  {
+    name: "Supply",
+    permissions: {
+      0: "Selecting a single station",
+      1: "Selecting all stations",
+      2: "Creating new supply",
+      3: "Print supply record",
     },
+  },
 
-    {
-        name: "Regulatory payment",
-        permissions: {
-            '0': "Selecting a single station",
-            '1': "Selecting all stations",
-            '2': "Registering a payment",
-            '3': "Print payment record",
-        }
+  {
+    name: "Regulatory payment",
+    permissions: {
+      0: "Selecting a single station",
+      1: "Selecting all stations",
+      2: "Registering a payment",
+      3: "Print payment record",
     },
+  },
 
-    {
-        name: "Tank Update",
-        permissions: {
-            '0': "Selecting a single station",
-            '1': "Selecting all stations",
-            '2': "Print tank update record",
-        }
+  {
+    name: "Tank Update",
+    permissions: {
+      0: "Selecting a single station",
+      1: "Selecting all stations",
+      2: "Print tank update record",
     },
+  },
 
-    {
-        name: "Human Resources",
-        permissions: {
-            '0': "Selecting a single station for employess",
-            '1': "Selecting all stations for employees",
-            '3': "Create new employees",
-            '4': "view all employees based on user types",
-            '5': "Print employee list",
-            '6': "Selecting a single station for salary",
-            '7': "Selecting all stations for salary",
-            '8': "Create new salary",
-            '9': "print report on salary",
-            '10': "Selecting a single station for query",
-            '11': "Selecting all stations for query",
-            '12': "Create new query",
-            '13': "print report on query",
-            '14': "Selecting a single station for attendance",
-            '15': "Selecting all stations for attendance",
-            '16': "Post new attendance",
-            '17': "print report on attendance",
-        }
+  {
+    name: "Human Resources",
+    permissions: {
+      0: "Selecting a single station for employess",
+      1: "Selecting all stations for employees",
+      3: "Create new employees",
+      4: "view all employees based on user types",
+      5: "Print employee list",
+      6: "Selecting a single station for salary",
+      7: "Selecting all stations for salary",
+      8: "Create new salary",
+      9: "print report on salary",
+      10: "Selecting a single station for query",
+      11: "Selecting all stations for query",
+      12: "Create new query",
+      13: "print report on query",
+      14: "Selecting a single station for attendance",
+      15: "Selecting all stations for attendance",
+      16: "Post new attendance",
+      17: "print report on attendance",
     },
+  },
 
-    {
-        name: "Settings",
-        permissions: {
-            '0': "Selecting a single station",
-            '1': "Selecting all stations",
-            '2': "Update my station",
-            '3': "Change permission enabled for a user",
-            '4': "Change appearance",
-            '5': "Change logo",
-            '6': "Change password",
-            '7': "Change email",
-            '8': "Delete my station",
-        }
-    }
-]
+  {
+    name: "Settings",
+    permissions: {
+      0: "Selecting a single station",
+      1: "Selecting all stations",
+      2: "Update my station",
+      3: "Change permission enabled for a user",
+      4: "Change appearance",
+      5: "Change logo",
+      6: "Change password",
+      7: "Change email",
+      8: "Delete my station",
+    },
+  },
+];
 
 export default data;
---
+++ src/components/common/selectstations.js
@@ -0,0 +1,149 @@
+import { MenuItem, Select } from "@mui/material";
+import { useState } from "react";
+import { useCallback } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import OutletService from "../../services/outletService";
+import { adminOutlet, getAllStations } from "../../storage/outlet";
+import swal from "sweetalert";
+
+const mobile = window.matchMedia("(max-width: 600px)");
+
+const SelectStation = ({ oneStation, allStation }) => {
+  const allOutlets = useSelector((state) => state.outlet.allOutlets);
+  const oneStationData = useSelector((state) => state.outlet.adminOutlet);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+
+  const [defaultState, setDefault] = useState(0);
+
+  const resolveUserID = () => {
+    if (user.userType === "superAdmin") {
+      return { id: user._id };
+    } else {
+      return { id: user.organisationID };
+    }
+  };
+
+  const getAllStationData = useCallback(() => {
+    const payload = {
+      organisation: resolveUserID().id,
+    };
+
+    if (oneStationData !== null) {
+      if (oneStation || allStation || user.userType === "superAdmin") {
+        const findID = allOutlets.findIndex(
+          (data) => data._id === oneStationData._id
+        );
+        setDefault(findID + 1);
+        return;
+      }
+    }
+
+    OutletService.getAllOutletStations(payload).then((data) => {
+      dispatch(getAllStations(data.station));
+      if (
+        (oneStation || user.userType === "superAdmin") &&
+        oneStationData === null
+      ) {
+        if (!allStation) setDefault(1);
+        dispatch(adminOutlet(null));
+        return "None";
+      } else {
+        OutletService.getOneOutletStation({ outletID: user.outletID }).then(
+          (data) => {
+            dispatch(adminOutlet(data.station));
+          }
+        );
+      }
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user._id, user.userType, user.outletID, dispatch]);
+
+  useEffect(() => {
+    getAllStationData();
+  }, [getAllStationData]);
+
+  const changeMenu = (index, item) => {
+    if (!allStation && item === null)
+      return swal("Warning!", "Permission denied", "info");
+    setDefault(index);
+    dispatch(adminOutlet(item));
+  };
+
+  return (
+    <div style={style}>
+      {oneStation && (
+        <Select
+          labelId="demo-select-small"
+          id="demo-select-small"
+          value={defaultState}
+          sx={selectStyle2}>
+          <MenuItem
+            onClick={() => {
+              changeMenu(0, null);
+            }}
+            style={menu}
+            value={0}>
+            All Stations
+          </MenuItem>
+          {allOutlets.map((item, index) => {
+            return (
+              <MenuItem
+                key={index}
+                style={menu}
+                onClick={() => {
+                  changeMenu(index + 1, item);
+                }}
+                value={index + 1}>
+                {item.outletName + ", " + item.alias}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      )}
+      {oneStation || (
+        <Select
+          labelId="demo-select-small"
+          id="demo-select-small"
+          value={0}
+          sx={selectStyle2}
+          disabled>
+          <MenuItem style={menu} value={0}>
+            {!oneStation
+              ? oneStationData?.outletName + ", " + oneStationData?.alias
+              : "No station created"}
+          </MenuItem>
+        </Select>
+      )}
+    </div>
+  );
+};
+
+const style = {
+  margin: mobile.matches ? "10px" : "0px",
+  marginLeft: mobile.matches ? "0px" : "10px",
+  marginRight: "0px",
+};
+
+const selectStyle2 = {
+  minWidth: mobile.matches ? "225px" : "120px",
+  maxWidth: "300px",
+  height: "30px",
+  borderRadius: "0px",
+  background: "#F2F1F1B2",
+  color: "#000",
+  fontSize: "12px",
+  outline: "none",
+  margin: "0px",
+  "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+    border: "1px solid #777777",
+  },
+};
+
+const menu = {
+  fontSize: "12px",
+};
+
+export default SelectStation;
