2529a36f4f6f90dadaf2881bca03599142ef2d3c
Amabills Technologies <Amabillstechnologies@Ahmards-MacBook-Air.local>
2024-01-08 09:17:41 +0100

    update

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BASE_URL: process.env.BASE_URL,
+  BASE_URL: process.env.REACT_APP_BASE_URL,
   // BASE_URL: process.env.REACT_APP_LOCAL,
   REPORTS_URL: process.env.REACT_APP_REPORTS,
   API_KEY: process.env.REACT_APP_APP_KEY,
